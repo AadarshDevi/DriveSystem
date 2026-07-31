aaf814b7552be15134d15b35c8e98efeeba19c9b
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-31 11:36:25 -0700

    function to print orientation

--- src/main.cpp
+++ src/main.cpp
@@ -189,6 +189,8 @@ void coords(void *pvParameters);
 
 void orient(void *pvParameters);
 
+void printOrientation(void *pvParameters);
+
 void setup() {
     Serial.begin(115200);
 
@@ -251,147 +253,30 @@ void orient(void *pvParameters) {
     }
 }
 
-// void imu(void *pvParameters) {
-//     // Initialize I2C
-//     Serial.println("[Before] Initializing Wire");
-//     Wire.begin(I2C_SCL_PIN, I2C_SDA_PIN, static_cast<int>(1E5)); // 1E5 = 100,000 or 100kHz to steadily upload code
-//     Serial.println("[After] Wire Initialized");
-//
-//     // Initialize IMU
-//     Serial.println("[Before] Initializing MPU6050");
-//     mpu.initialize();
-//     if (!mpu.testConnection()) {
-//         Serial.println("[Error] MPU6050 Connection Failed");
-//         vTaskDelete(NULL);
-//     }
-//     Serial.println("[After] MPU6050 Initialized");
-//
-//     // IMU Reset
-//     Serial.println("[Before] Resetting MPU6050");
-//     mpu.setXAccelOffset(0);
-//     mpu.setYAccelOffset(0);
-//     mpu.setZAccelOffset(0);
-//     mpu.setXGyroOffset(0);
-//     mpu.setYGyroOffset(0);
-//     mpu.setZGyroOffset(0);
-//     Serial.println("[After] MPU6050 Reset");
-//
-//     // IMU Auto Calibration
-//     Serial.println("[Before] Calibrating MPU6050");
-//     mpu.CalibrateAccel(6);
-//     mpu.CalibrateGyro(6);
-//     mpu.PrintActiveOffsets();
-//     mpu.setDLPFMode(MPU6050_DLPF_BW_20); // low pass band filter
-//     Serial.println("[After] MPU6050 Calibrated");
-//
-//     // Initialize Onboard DMP
-//     Serial.println("[Before] Initializing DMP");
-//     int dev_status = mpu.dmpInitialize();
-//     if (dev_status != 0) {
-//         Serial.println("[Error] DMP Initialization Failed");
-//         vTaskDelete(NULL);
-//     }
-//     mpu.setDMPEnabled(true); // enable mpu's dmp
-//     packet_size = mpu.dmpGetFIFOPacketSize(); // dmp packet size
-//     dmp_ready = true; // dmp is ready to be used
-//
-//     // DMP is ready to start doing calculations really quickly.
-//     // so i2c can now be faster
-//     Wire.setClock(static_cast<int>(4E5)); // 4E5 = 400,000 kHz
-//     Serial.println("[After] DMP Initialized");
-//
-//     // Stabilize MPU605
-//     Serial.println("[Before] Stabilizing MPU6050");
-//     Orientation_t previous = {.roll = 0.0f, .pitch = 0.0f, .yaw = 0.0f};
-//     Orientation_t current;
-//     constexpr int MAX_SAMPLES = 80;
-//     int samples = 0;
-//
-//     vTaskDelay(pdMS_TO_TICKS(10000)); // 3 sec delay to stabilize mpu before setting up stable orientation
-//     while (samples < MAX_SAMPLES) {
-//         if (!mpu.dmpGetCurrentFIFOPacket(fifo_buffer)) {
-//             vTaskDelay(pdMS_TO_TICKS(2));
-//             continue;
-//         }
-//
-//         // Compute 4D Orientation
-//         // Compute Roll Pitch
-//         // Compute 3D Orientation
-//         mpu.dmpGetQuaternion(&quaternion, fifo_buffer);
-//         mpu.dmpGetGravity(&gravity, &quaternion);
-//         mpu.dmpGetYawPitchRoll(ypr, &quaternion, &gravity);
-//
-//         // Compute 3D Orientation from 4D Orientation
-//         // Convert rad to deg
-//         current.yaw = ypr[0] * 180.0f / M_PI;
-//         current.pitch = ypr[1] * 180.0f / M_PI;
-//         current.roll = ypr[2] * 180.0f / M_PI;
-//
-//         if (
-//             fabs(current.roll - previous.roll) >= 0.02f ||
-//             fabs(current.pitch - previous.pitch) >= 0.02f ||
-//             fabs(current.yaw - previous.yaw) >= 0.02f
-//         ) {
-//             samples = 0;
-//             previous = current;
-//             vTaskDelay(pdMS_TO_TICKS(10));
-//             continue;
-//         }
-//
-//         samples++;
-//         previous = current;
-//         vTaskDelay(pdMS_TO_TICKS(10));
-//     }
-//
-//     mpu.dmpGetQuaternion(&quaternion, fifo_buffer);
-//     mpu.dmpGetGravity(&gravity, &quaternion);
-//     mpu.dmpGetYawPitchRoll(ypr, &quaternion, &gravity);
-//
-//     // Compute 3D Orientation from 4D Orientation
-//     // Convert rad to deg
-//     base_orientation.yaw = ypr[0] * 180.0f / M_PI;
-//     base_orientation.pitch = ypr[1] * 180.0f / M_PI;
-//     base_orientation.roll = ypr[2] * 180.0f / M_PI;
-//     Serial.println("[After] MPU6050 Stabilized");
-//
-//     // Ready and compute orientation
-//     float roll = 0.0;
-//     float pitch = 0.0;
-//     float yaw = 0.0;
-//
-//     Serial.println("[Log] Reading Orientation");
-//     for (;;) {
-//         if (!(dmp_ready && mpu.dmpGetCurrentFIFOPacket(fifo_buffer))) {
-//             vTaskDelay(pdMS_TO_TICKS(10));
-//             continue;
-//         }
-//
-//         // Compute 4D Orientation
-//         mpu.dmpGetQuaternion(&quaternion, fifo_buffer);
-//         mpu.dmpGetGravity(&gravity, &quaternion);
-//         mpu.dmpGetYawPitchRoll(ypr, &quaternion, &gravity);
-//
-//         // Compute 3D Orientation from 4D Orientation
-//         // Convert rad to deg
-//         yaw = ypr[0] * 180.0f / M_PI;
-//         pitch = ypr[1] * 180.0f / M_PI;
-//         roll = ypr[2] * 180.0f / M_PI;
-//
-//         current_orientation.roll = roll - base_orientation.roll;
-//         current_orientation.pitch = pitch - base_orientation.pitch;
-//         current_orientation.yaw = yaw - base_orientation.yaw;
-//
-//         Serial.printf(
-//             "Roll:  %6.2f° | Pitch: %6.2f° | Yaw: %6.2f°\n",
-//             current_orientation.roll,
-//             current_orientation.pitch,
-//             current_orientation.yaw
-//         );
-//
-//         // ~100Hz
-//         vTaskDelay(pdMS_TO_TICKS(10));
-//     }
-// }
+void printOrientation(void *pvParameters) {
+    float roll = 0.0f;
+    float pitch = 0.0f;
+    float yaw = 0.0f;
+    bool print;
+
+    for (;;) {
+        print = false;
+        if (xSemaphoreTake(mutex, pdMS_TO_TICKS(MUTEX_TIMEOUT_MS)) == pdTRUE) {
+            roll = current_orientation.roll;
+            pitch = current_orientation.pitch;
+            yaw = current_orientation.yaw;
+            print = true;
+            xSemaphoreGive(mutex);
+        }
+
+        if (print) {
+            Serial.printf("Roll:  %6.2f° | Pitch: %6.2f° | Yaw: %6.2f°\n", roll, pitch, yaw);
+        }
+        vTaskDelay(pdMS_TO_TICKS(30));
+    }
+}
+
+
 void loop() {
     vTaskDelete(nullptr);
 }
