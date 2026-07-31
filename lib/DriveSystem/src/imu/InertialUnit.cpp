b314c8f4361a559861122157506efcc5a51146d3
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-30 22:09:30 -0700

    class InterialUnit: stabalize mpu readings to get orientation close to 0 in all axis

--- lib/DriveSystem/src/imu/InertialUnit.cpp
+++ lib/DriveSystem/src/imu/InertialUnit.cpp
@@ -21,3 +21,40 @@ void InertialUnit::reset() {
     mpu.setZGyroOffset(0);
     Serial.println("[After] MPU6050 Reset");
 }
+
+void InertialUnit::stabilize() {
+    // Stabilize MPU605
+    Serial.println("[Before] Stabilizing MPU6050");
+    Orientation_t previous = {.roll = 0.0f, .pitch = 0.0f, .yaw = 0.0f};
+    Orientation_t current = {.roll = 0.0f, .pitch = 0.0f, .yaw = 0.0f};
+    constexpr int MAX_SAMPLES = 80;
+    int samples = 0;
+
+    vTaskDelay(pdMS_TO_TICKS(10000)); // 3 sec delay to stabilize mpu before setting up stable orientation
+    while (samples < MAX_SAMPLES) {
+        if (!mpu.dmpGetCurrentFIFOPacket(fifo_buffer)) {
+            vTaskDelay(pdMS_TO_TICKS(2));
+            continue;
+        }
+
+        current = getOrientation();
+
+        if (
+            fabs(current.roll - previous.roll) >= 0.02f ||
+            fabs(current.pitch - previous.pitch) >= 0.02f ||
+            fabs(current.yaw - previous.yaw) >= 0.02f
+        ) {
+            samples = 0;
+            previous = current;
+            vTaskDelay(pdMS_TO_TICKS(10));
+            continue;
+        }
+
+        samples++;
+        previous = current;
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+
+    base_orientation = getOrientation();
+    Serial.println("[After] MPU6050 Stabilized");
+}
