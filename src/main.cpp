a3f72bad2610f94e97a7e09f25766e480fedd546
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-30 19:07:16 -0700

    I2C Initialization

--- src/main.cpp
+++ src/main.cpp
@@ -204,4 +204,8 @@ void loop() {
 }
 
 void imu(void *pvParameters) {
+    // Initialize I2C
+    Serial.println("[Before] Initializing Wire");
+    Wire.begin(I2C_SCL_PIN, I2C_SDA_PIN, 1E5); // 1E5 = 100,000 or 100kHz to steadily upload code
+    Serial.println("[After] Wire Initialized");
 }
