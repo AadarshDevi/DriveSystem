b67284ac32110dae4051efb85feeda84d745839d
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-31 11:34:29 -0700

    imports removed

--- src/main.cpp
+++ src/main.cpp
@@ -133,10 +133,8 @@
 
 #include <Arduino.h>                        // Arduino Lib
 #include <motor/RawMotor.h>                 // Custom Motor
-#include <freertos/FreeRTOS.h>              // Parallel Processing
+#include <imu/InertialUnit.h>               // Custom IMU
 #include <MPU6050_6Axis_MotionApps20.h>     // MPU-6050 Lib
-#include <Wire.h>                           // I2C Hardware Driver
-#include <I2Cdev.h>                         // I2C Auto Transmission Layer aka Wrapper
 #include <SystemMode.h>                     // Type of Data Transfer
 #include <DriveMode.h>                      // Type of Driving
 
