da9bca9ff4c404912dd7bddd261cb20bb15583c8
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-30 19:10:20 -0700

    enum to check the state of the Powertrain control by the main computer

---
+++ lib/DriveSystem/include/SystemMode.h
@@ -0,0 +1,24 @@
+//
+// Created by CryosArtic on 7/30/2026.
+//
+
+#ifndef DRIVESYSTEM_SYSTEMMODE_H
+#define DRIVESYSTEM_SYSTEMMODE_H
+
+/*
+ SystemMode tells what type of control & connection the main computer will have on the powertrain
+ */
+enum SystemMode {
+    /*
+     This allows the Powertrain to be independent from the Main Computer
+     */
+    AUTONOMOUS,
+
+    /*
+     The Powertrain is handicapped by the IMU or Wheel encoder. Because of that, the Powertrain is dependent on the main computer
+     */
+    GUIDED
+};
+
+
+#endif //DRIVESYSTEM_SYSTEMMODE_H
