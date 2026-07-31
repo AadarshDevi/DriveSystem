6ffcaadc1329fd0cd30eeb3d6fa014e9ada5cef3
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-30 20:38:46 -0700

    setting a lower max speed for testing

--- src/main.cpp
+++ src/main.cpp
@@ -166,7 +166,7 @@ Orientation_t target_orientation = {.roll = 0.0f, .pitch = 0.0f, .yaw = 0.0f}; /
 // Motor > Power
 int power = 128; // motor power
 constexpr int absMinPower = 50; // min power limit
-constexpr int absMaxPower = 255; // max power limit
+constexpr int absMaxPower = 175; // max power limit
 constexpr int shutdownPower = 0; // shutdown motor power - for emergency uses
 
 // Motor
