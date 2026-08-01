cbdbf9777d7f56cb5f11a55b86fe6c3a864488b3
Aadarsh D <aadarsh123456char@gmail.com>
2026-08-01 16:30:17 -0700

    coordinates list header file updated

--- lib/DriveSystem/include/navigation/CoordinateList.h
+++ lib/DriveSystem/include/navigation/CoordinateList.h
@@ -21,21 +21,29 @@ typedef struct {
 
 class CoordinateList {
 private:
-    const Coordinate_t target_coords[SIZE];
-    int index = 0;
+    Coordinate_t target_coords[SIZE];
+    int data_index = 0;
+    int target_index = 0;
 
 public:
-    void add(float x, float y, float z);
+    bool add(float x, float y, float z);
 
-    void add(Coordinate_t coord);
+    bool add(const Coordinate_t &coord);
 
-    Coordinate_t getTarget() const;
+    Coordinate_t getTarget();
 
     Coordinate_t getFinalTarget() const;
 
     int getSize() const { return SIZE; }
 
     void clear();
+
+    bool final_target_reached() {
+        if (target_index == data_index) {
+            return true;
+        }
+        return false;
+    }
 };
 
 
