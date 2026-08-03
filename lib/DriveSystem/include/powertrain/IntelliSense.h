1f375a63b8eb9193392d65b40db814f8f19a8310
Aadarsh D <aadarsh123456char@gmail.com>
2026-08-03 14:00:33 -0700

    Renamed DriveSystem to IntelliSense

---
+++ lib/DriveSystem/include/powertrain/IntelliSense.h
@@ -0,0 +1,12 @@
+//
+// Created by CryosArtic on 6/27/2026.
+//
+
+#ifndef DRIVESYSTEM_DRIVESYSTEM_H
+#define DRIVESYSTEM_DRIVESYSTEM_H
+
+class IntelliSense {
+};
+
+
+#endif //DRIVESYSTEM_DRIVESYSTEM_H
