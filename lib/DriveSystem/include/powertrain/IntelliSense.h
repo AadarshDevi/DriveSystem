b545df905af4a59e2719d0e80b1114f739779552
Aadarsh D <aadarsh123456char@gmail.com>
2026-08-04 19:42:47 -0700

    updated #define and added all the needed libs

--- lib/DriveSystem/include/powertrain/IntelliSense.h
+++ lib/DriveSystem/include/powertrain/IntelliSense.h
@@ -2,8 +2,11 @@
 // Created by CryosArtic on 6/27/2026.
 //
 
-#ifndef DRIVESYSTEM_DRIVESYSTEM_H
-#define DRIVESYSTEM_DRIVESYSTEM_H
+#ifndef DRIVESYSTEM_INTELLISENSE_H
+#define DRIVESYSTEM_INTELLISENSE_H
+
+#include "comms/InputManager.h"
+#include "DifferentialDrive.h"
 #include "sensor/WheelEncoder.h"
 #include "sensor/InertialUnit.h"
 
