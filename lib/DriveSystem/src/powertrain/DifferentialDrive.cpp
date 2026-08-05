a227e8a721e8c2585ed076430937f0da56766cc4
Aadarsh D <aadarsh123456char@gmail.com>
2026-08-04 19:48:56 -0700

    reimplemented DifferentialDrive.cpp with new code which replaced old code. not update, but replaced with better

--- lib/DriveSystem/src/powertrain/DifferentialDrive.cpp
+++ lib/DriveSystem/src/powertrain/DifferentialDrive.cpp
@@ -9,83 +9,62 @@
 #include "motor/RawMotor.h"
 
 DifferentialDrive::DifferentialDrive(
-    int new_wheelCount,
-    std::vector<std::vector<int> > new_motorPins,
-    std::vector<int> new_powerInput
-) {
-    int wheelCount = -1;
-
-    if (new_wheelCount % 2 != 0) {
-        new_wheelCount = new_wheelCount - 1;
-    }
-
-    if (new_wheelCount <= 0) {
-        wheelCount = 4;
-    }
-
-    int power = new_powerInput.at(0);
-    int minPower = new_powerInput.at(0);
-    int maxPower = new_powerInput.at(0);
-    int shutdownPower = new_powerInput.at(0);
-
-    for (int i = 0; i < wheelCount; i++) {
-        std::vector<int> motorPins = new_motorPins[i];
-        RawMotor motor(
-            motorPins.at(0), motorPins.at(1), motorPins.at(2),
-            power, minPower, maxPower, shutdownPower
-        );
-        motor.setPower(power);
-        motors.push_back(motor);
-    }
+    RawMotor &left_omni, RawMotor &right_omni,
+    RawMotor &left_mecanum, RawMotor &right_mecanum
+) : left_omni(left_omni), right_omni(right_omni), left_mecanum(left_mecanum), right_mecanum(right_mecanum) {
+    setPower(100);
 }
 
-void DifferentialDrive::debugPower(int powerInput) const {
-    for (int i = 0; i < motors.size(); i++) {
-        RawMotor motor = motors[i];
-        Serial.printf(
-            "ippwr%d:%d pwr_%d:%d minp_%d:%d maxp_%d:%d stdpwr_%d:%d ",
-            i + 1, powerInput,
-            i + 1, motor.getPower(),
-            i + 1, motor.getAbsMinPower(),
-            i + 1, motor.getAbsMaxPower(),
-            i + 1, motor.getShutdownPower()
-        );
-    }
-    Serial.println();
+void DifferentialDrive::setPower(int power) {
+    left_omni.setPower(power);
+    right_omni.setPower(power);
+    left_mecanum.setPower(power);
+    right_mecanum.setPower(power);
 }
 
-void DifferentialDrive::setPower(int new_power) const {
-    for (RawMotor motor: motors) {
-        motor.setPower(new_power);
+void DifferentialDrive::drive(float distance) {
+    if (distance > 0) {
+        left_omni.forward();
+        right_omni.forward();
+        left_mecanum.forward();
+        right_mecanum.forward();
+    } else {
+        left_omni.reverse();
+        right_omni.reverse();
+        left_mecanum.reverse();
+        right_mecanum.reverse();
     }
+    run();
 }
 
-void DifferentialDrive::setPower(int new_motorNum, int new_power) {
-    motors[new_motorNum].setPower(new_power);
+void DifferentialDrive::rotate(float angle) {
+    if (angle > 0) {
+        // right
+        left_omni.forward();
+        right_omni.reverse();
+        left_mecanum.forward();
+        right_mecanum.reverse();
+    } else {
+        // left
+        right_omni.forward();
+        left_omni.reverse();
+        right_mecanum.forward();
+        left_mecanum.reverse();
+    }
+    // code
+    run();
 }
 
-void DifferentialDrive::shutdownMotors() {
-    for (RawMotor motor: motors) {
-        while (motor.getPower() > motor.getShutdownPower()) {
-            motor.powerVal(
-                (motor.getPower() < (motor.getAbsMaxPower() * 0.05))
-                    ? motor.getShutdownPower()
-                    : motor.getPower() * 0.75);
-            analogWrite(motor.getPinPWM(), motor.getPower());
-            motor.debugAllPower(0);
-            delay(200);
-        }
-    }
-}
-
-void DifferentialDrive::debug(bool new_debug) {
-    for (RawMotor motor: motors) {
-        motor.debug(new_debug);
-    }
+void DifferentialDrive::stop() {
+    left_omni.stop();
+    right_omni.stop();
+    left_mecanum.stop();
+    right_mecanum.stop();
 }
 
 void DifferentialDrive::run() {
-    for (RawMotor motor: motors) {
-        motor.run();
-    }
+    left_omni.run();
+    right_omni.run();
+    left_mecanum.run();
+    right_mecanum.run();
 }
