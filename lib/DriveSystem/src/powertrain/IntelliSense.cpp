dc6320e961d285588d29472f0de79c1ee8781f8c
Aadarsh D <aadarsh123456char@gmail.com>
2026-08-04 19:43:36 -0700

    IntelliSense.cpp method bodies written

--- lib/DriveSystem/src/powertrain/IntelliSense.cpp
+++ lib/DriveSystem/src/powertrain/IntelliSense.cpp
@@ -3,3 +3,51 @@
 //
 
 #include "../../include/powertrain/IntelliSense.h"
+
+#include "comms/InputManager.h"
+#include "powertrain/DifferentialDrive.h"
+#include "sensor/InertialUnit.h"
+#include "sensor/WheelEncoder.h"
+
+IntelliSense::IntelliSense(
+    InertialUnit &inertialUnit,
+    WheelEncoder &wheelEncoder,
+    InputManager &inputManager,
+    DifferentialDrive &differentialDrive
+) : inertialUnit(inertialUnit),
+    wheelEncoder(wheelEncoder),
+    inputManager(inputManager),
+    differentialDrive(differentialDrive) {
+}
+
+void IntelliSense::run() {
+    for (;;) {
+        if (!inputManager.hasNewData()) {
+            vTaskDelay(pdMS_TO_TICKS(10));
+            continue;
+        }
+
+        Trajectory2_t trajectory = inputManager.getNewData();
+        Serial.printf("Angle:%.2f", trajectory.angle);
+        Serial.printf("\tDistance:%.2f", trajectory.distance);
+
+        float target_angle = inertialUnit.getOrientation().yaw + trajectory.angle;
+        const float delta_angle = 2.3f;
+        while (
+            inertialUnit.getOrientation().yaw > target_angle - delta_angle &&
+            inertialUnit.getOrientation().yaw < target_angle + delta_angle
+        ) {
+            differentialDrive.rotate(target_angle);
+        }
+        differentialDrive.stop();
+
+        float target_distance = trajectory.distance;
+        // const float delta_dist = 2.3f; // cm
+        while (target_distance < wheelEncoder.getDistanceTravelled()) {
+            differentialDrive.drive(target_distance);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+    // code
+}
